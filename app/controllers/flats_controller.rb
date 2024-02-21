cb7d4008205f17db00190222baf9340cd6207ab0
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-02-21 20:53:25 +0100

    added create and going back to menu on taskbar

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,12 +4,17 @@ class FlatsController < ApplicationController
   end
 
   def show
+    @flat = Flat.find(params[:id])
   end
 
   def new
+    @flat = Flat.new
   end
 
   def create
+    @flat = Flat.new(params_flat)
+    @flat.save!
+    redirect_to flat_path(@flat)
   end
 
   def edit
@@ -19,5 +24,11 @@ class FlatsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def params_flat
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
